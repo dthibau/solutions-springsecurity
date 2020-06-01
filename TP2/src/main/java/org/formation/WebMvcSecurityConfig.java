de356dee7eaf8092c2cb41db96f985faa425ee0a
David THIBAU <david.thibau@gmail.com>
2020-06-01 17:03:50 +0200

    Solution 4.2

--- TP2/src/main/java/org/formation/WebMvcSecurityConfig.java
+++ TP2/src/main/java/org/formation/WebMvcSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -39,11 +40,12 @@ public class WebMvcSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeRequests()
+		http.csrf().disable()
+		.authorizeRequests()
 		.antMatchers("/fournisseurs*").hasRole("MANAGER")
 		.antMatchers("/produits*").hasAnyRole("PRODUCT_MANAGER","MANAGER")
 		.antMatchers("/swagger-ui.html","/swagger-resources/**","/v2/api-docs/**").permitAll()
-		.antMatchers("/api/*").permitAll()
+		.antMatchers("/api/**").permitAll()
 		.antMatchers("/actuator/**").permitAll()
 		.anyRequest().authenticated().and().formLogin().and().logout().logoutUrl("/logout").invalidateHttpSession(true)
 		.permitAll();
