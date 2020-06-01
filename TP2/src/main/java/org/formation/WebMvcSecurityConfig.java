15a301be67f3fd7061dfbf110f3d309a7c9b00f0
David THIBAU <david.thibau@gmail.com>
2020-06-01 10:00:54 +0200

    Solutions 2.4

--- TP2/src/main/java/org/formation/WebMvcSecurityConfig.java
+++ TP2/src/main/java/org/formation/WebMvcSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,7 +20,20 @@ public class WebMvcSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	UserDetailsService userDetailService;
 
+	
 
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(http);
+		
+		http.logout().logoutUrl("/logout")                   
+		  .logoutSuccessUrl("/")             
+		  .invalidateHttpSession(true);
+		
+		System.out.println(http.logout());
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
