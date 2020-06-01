a44352c36ea2cd79d9614a0913defd6d1d0a8576
David THIBAU <david.thibau@gmail.com>
2020-06-01 14:52:08 +0200

    Fix 2.2

--- TP2/src/main/resources/import.sql
+++ TP2/src/main/resources/import.sql
@@ -7,8 +7,8 @@ insert into produit (availability,description,hauteur,largeur, longueur,nom,prix
 insert into produit (availability,description,hauteur,largeur, longueur,nom,prix_unitaire,reference,fournisseur_id) values (1, 'bli-bli', 150, 210, 300, 'Godishus', 79.95, 'GOD79', 2);
 
 -- Rôles
-insert into role (name) values ('PRODUCT_MANAGER');
-insert into role (name) values ('MANAGER');
+insert into role (name) values ('ROLE_PRODUCT_MANAGER');
+insert into role (name) values ('ROLE_MANAGER');
 
 -- Mot de passe BCrypt 'secret'
 insert into member (created, disabled, email, first_name, last_name, login , password,role_id) values ('2019-05-01', null, 'pmanager@grocery.com', 'Mr', 'ProductManager', 'pmanager', '$2a$10$aK81fm.BSg13nxvjEKcEfeD/3cCphRLVCHiQgyhvX1tHYLkH309RC',1);
